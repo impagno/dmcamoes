376b7f08c83a46c0f40d4b6af40527000c4539e0
ipagnoncelli]@gmail.com <Ci141080!>
2018-04-10 11:41:17 -0300

    Versão incial

---
+++ SalvandoDados/app/src/main/java/edu/camoes/dm/salvandodados/InternoActivity.java
@@ -0,0 +1,13 @@
+package edu.camoes.dm.salvandodados;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InternoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interno);
+    }
+}
