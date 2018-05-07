bc288cf1f42b58eaa1e643a952135423642be58c
ipagnoncelli]@gmail.com <Ci141080!>
2018-05-07 15:47:20 -0300

    Demonstração de Fragment

--- CadastrodePessoa/app/src/main/java/edu/impagno/cadastrodepessoa/MainActivity.java
+++ CadastrodePessoa/app/src/main/java/edu/impagno/cadastrodepessoa/MainActivity.java
@@ -1,10 +1,11 @@
 package edu.impagno.cadastrodepessoa;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PessoaFisica.OnFragmentInteractionListener, PessoaJuridica.OnFragmentInteractionListener {
 
     private RadioGroup radio;
     private int flagPessoa;
@@ -30,8 +31,22 @@ public class MainActivity extends AppCompatActivity {
         radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-
+                switch(checkedId) {
+                    case R.id.rbPF:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frmLayout, new PessoaFisica()).commit();
+                        break;
+                    case R.id.rbPJ:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frmLayout, new PessoaJuridica()).commit();
+                        break;
+                }
             }
         });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
